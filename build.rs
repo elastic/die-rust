c9ff807c5eb34fff27aac2932ad158249ce93f08
Christophe Alladoum <85187342+calladoum-elastic@users.noreply.github.com>
2025-01-09 16:16:16 -0800

    Add Windows build support (#3)
    # Description
    
    Adds Windows support for crate
    Including CI testing
    
    Tested with Qt6 6.2.2
    
    Requires Qt6 DLLs in library search path
     - Qt6Network.dll
     - Qt6Qml.dll
     - Qt6Core.dll

--- build.rs
+++ build.rs
@@ -1,95 +1,171 @@
 // build.rs
 // https://doc.rust-lang.org/cargo/reference/build-scripts.html
 
-fn main() {
-    let base_dir = "./libdie++";
-    let build_dir = "./libdie++/build";
-    let install_dir = "./libdie++/install";
-    let lib_die_path = "./libdie++/build/_deps/dielibrary-build/src";
+const BASE_DIR: &'static str = "./libdie++";
+#[allow(dead_code)]
+const BUILD_DIR: &'static str = "./libdie++/build";
+const INSTALL_DIR: &'static str = "./libdie++/install";
+const LIB_DIE_PATH: &'static str = "./libdie++/build/_deps/dielibrary-build/src";
 
+#[cfg(target_os = "windows")]
+const MSVC_PATH: &'static str = r"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22000.0";
+
+#[cfg(debug_assertions)]
+const BUILD_TYPE: &'static str = "Debug";
+#[cfg(not(debug_assertions))]
+const BUILD_TYPE: &'static str = "Release";
+
+fn cmake_build_die() {
     // CMake configure
     {
-        std::process::Command::new("cmake")
-            .args(["-S", base_dir])
-            .args(["-B", build_dir])
+        assert!(std::process::Command::new("cmake")
+            .current_dir(BASE_DIR)
+            .args(["-S", "."])
+            .args(["-B", "build"])
             .spawn()
             .unwrap()
             .wait()
-            .expect("failed to configure cmake");
+            .expect("failed to configure cmake")
+            .success());
     }
 
     // CMake build
     {
         let nb_cpu = "4";
 
-        std::process::Command::new("cmake")
-            .args(["--build", build_dir])
+        assert!(std::process::Command::new("cmake")
+            .args(["--build", "build"])
             .args(["-j", nb_cpu])
+            .args(["--config", BUILD_TYPE])
+            .current_dir(BASE_DIR)
             .spawn()
             .unwrap()
             .wait()
-            .expect("failed to build with cmake");
+            .expect("failed to build with cmake")
+            .success());
     }
 
     // CMake install
     {
-        std::process::Command::new("cmake")
-            .args(["--install", build_dir])
-            .args(["--prefix", install_dir])
-            .args(["--strip"])
+        assert!(std::process::Command::new("cmake")
+            .args(["--install", "build"])
+            .args(["--config", BUILD_TYPE])
+            .args(["--prefix", "install"])
+            .current_dir(BASE_DIR)
             .spawn()
             .unwrap()
             .wait()
-            .expect("failed to install with cmake");
+            .expect("failed to install with cmake")
+            .success());
     }
+}
 
-    // die++
-    println!("cargo:rustc-link-search=native={}", build_dir);
+fn install_common() {
+    // die & die++
     println!("cargo:rustc-link-lib=static=die++");
-
-    // die
-    println!("cargo:rustc-link-search=native={}/dielib", lib_die_path);
     println!("cargo:rustc-link-lib=static=die");
 
     // die 3rd party
-    println!(
-        "cargo:rustc-link-search=native={}/XArchive/3rdparty/bzip2",
-        lib_die_path
-    );
     println!("cargo:rustc-link-lib=static=bzip2");
-    println!(
-        "cargo:rustc-link-search=native={}/XArchive/3rdparty/lzma",
-        lib_die_path
-    );
     println!("cargo:rustc-link-lib=static=lzma");
-    println!(
-        "cargo:rustc-link-search=native={}/XArchive/3rdparty/zlib",
-        lib_die_path
-    );
     println!("cargo:rustc-link-lib=static=zlib");
-
-    println!("cargo:rustc-link-search=native={}/XCapstone", lib_die_path);
     println!("cargo:rustc-link-lib=static=capstone_x86");
 
     // qt
-    if let Ok(qt_lib_path) = std::env::var("QT6_LIB_PATH") {
-        // let lib_qt_path = "./libdie++/build/6.2.2/gcc_64/lib";
+    println!("cargo:rerun-if-env-changed=QT6_LIB_PATH");
+    if let Some(qt_lib_path) = option_env!("QT6_LIB_PATH") {
         println!("cargo:rustc-link-search=native={}", qt_lib_path);
     }
 
+    if BUILD_TYPE == "Release" {
+        println!("cargo:rustc-link-lib=static=Qt6Core");
+        println!("cargo:rustc-link-lib=static=Qt6Qml");
+        println!("cargo:rustc-link-lib=static=Qt6Network");
+
+        println!("cargo:rustc-link-lib=dylib=Qt6Core");
+        println!("cargo:rustc-link-lib=dylib=Qt6Qml");
+        println!("cargo:rustc-link-lib=dylib=Qt6Network");
+    }
+}
+
+#[cfg(target_os = "linux")]
+fn install_linux() {
+    println!("cargo:rustc-link-search=native={}/die", INSTALL_DIR);
+    println!("cargo:rustc-link-search=native={}/die/lib", INSTALL_DIR);
+    println!("cargo:rustc-link-lib=dylib=stdc++");
     println!("cargo:rustc-link-lib=dylib=Qt6Core");
     println!("cargo:rustc-link-lib=dylib=Qt6Qml");
+    println!("cargo:rustc-link-lib=dylib=Qt6Network");
+    println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
 
-    let target = std::env::var("TARGET").unwrap();
+    println!("cargo:rustc-link-search=native={}/XCapstone", LIB_DIE_PATH);
+    for _mod in ["bzip2", "lzma", "zlib"].iter() {
+        println!(
+            "cargo:rustc-link-search=native={}/XArchive/3rdparty/{}",
+            LIB_DIE_PATH, _mod
+        );
+    }
+}
 
-    if target.contains("linux") {
-        println!("cargo:rustc-link-lib=dylib=stdc++");
-        println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
-    } else if target.contains("apple") {
-        println!("cargo:rustc-link-lib=dylib=c++");
-        println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
+#[cfg(target_os = "macos")]
+fn install_macos() {
+    todo!();
+    println!("cargo:rustc-link-lib=dylib=c++");
+    println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
+}
+
+#[cfg(target_os = "windows")]
+fn install_windows() {
+    println!("cargo:rustc-link-search=native={}/die/dielib", INSTALL_DIR);
+    println!(
+        "cargo:rustc-link-search=native={}/{}",
+        BUILD_DIR, BUILD_TYPE
+    );
+    println!(
+        "cargo:rustc-link-search=native={}/_deps/dielibrary-build/src/dielib/{}",
+        BUILD_DIR, BUILD_TYPE
+    );
+    for _mod in ["bzip2", "lzma", "zlib"].iter() {
+        println!(
+            "cargo:rustc-link-search=native={}/XArchive/3rdparty/{}/{}",
+            LIB_DIE_PATH, _mod, BUILD_TYPE
+        );
+    }
+    println!(
+        "cargo:rustc-link-search=native={}/XCapstone/{}",
+        LIB_DIE_PATH, BUILD_TYPE
+    );
+    println!("cargo:rustc-link-lib=dylib=Crypt32");
+    println!("cargo:rustc-link-lib=dylib=Wintrust");
+
+    if BUILD_TYPE == "Debug" {
+        println!("cargo:rustc-link-lib=static=Qt6Cored");
+        println!("cargo:rustc-link-lib=static=Qt6Qmld");
+        println!("cargo:rustc-link-lib=static=Qt6Networkd");
+        println!("cargo:rustc-link-lib=dylib=Qt6Cored");
+        println!("cargo:rustc-link-lib=dylib=Qt6Qmld");
+        println!("cargo:rustc-link-lib=dylib=Qt6Networkd");
+
+        println!("cargo:rustc-link-search=native={}/ucrt/x64", MSVC_PATH);
+        println!("cargo:rustc-link-lib=static=ucrtd");
+    }
+}
+
+fn main() {
+    cmake_build_die();
+
+    install_common();
+
+    if cfg!(target_os = "linux") {
+        #[cfg(target_os = "linux")]
+        install_linux();
+    } else if cfg!(target_os = "macos") {
+        #[cfg(target_os = "macos")]
+        install_macos();
+    } else if cfg!(target_os = "windows") {
+        #[cfg(target_os = "windows")]
+        install_windows();
     } else {
-        // TODO (calladoum) other OSes
         unimplemented!();
     }
 
